6765bb86820e53bb3cef2afc867d59a1d1261a28
maialen <maialenmunoa@gmail.com>
2024-04-13 00:10:40 +0200

    creating player

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const scaleFactor = 4;
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { k } from "./kaboomCtx";
+import { scaleFactor } from "./constants";
 
 k.loadSprite("spritesheet", "./spritesheet.png", {
     sliceX: 39,
@@ -13,4 +14,51 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
     },
 });
   
+k.loadSprite("map", "./map.png");
 
+k.setBackground (k.Color.fromHex("#311047"));
+
+k.scene("main", async () => {
+    const mapData = await (await fetch("./map.json")).json()
+    const layers = mapData.layers;
+
+    const map = k.make([
+        k.sprite("map"),
+        k.pos(0),
+        k.scale(scaleFactor)
+    ])
+
+    const player = k.make ([
+        k.sprite("spritesheet", { anim: "idle-down" }),
+        k.area({
+            shape: new k.Rect(k.vec2(0, 3), 10, 10),
+        }),
+        k.body(),
+        k.anchor("center"),
+        k.pos(),
+        k.scale(scaleFactor),
+        {
+            speed: 250,
+            direction: "down",
+            isInDialogue: false,
+        },
+        "player",
+    ]);
+
+    for (const layer of layers) {
+        if (layer.name === "boundaries") {
+            for (const boundary of layer.objects) {
+                map.add([
+                  k.area({
+                    shape: new k.Rect(k.vec2(0, 0), boundary.width, boundary.height),
+                  }),
+                    k.body({ isStatic: true }),  
+                    k.pos(boundary.x, boundary.y),
+                    boundary.name,
+                ]);
+            }
+        }
+    }
+});
+
+k.go("main");
