5bae320c60984914de459f1032cd4ef0e6053dcd
maialen <maialenmunoa@gmail.com>
2024-04-13 00:50:20 +0200

    adding player

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { k } from "./kaboomCtx";
 import { scaleFactor } from "./constants";
+import { displayDialogue } from "./utils";
 
 k.loadSprite("spritesheet", "./spritesheet.png", {
     sliceX: 39,
@@ -60,12 +61,32 @@ k.scene("main", async () => {
                 if(boundary.name) {
                     player.onCollide(boundary.name, () => {
                         player.isInDialogue = true;
-                        // TODO
+                        displayDialogue("TODO", () => {
+                            player.isInDialogue = false;
+                        })
                     });
+                }
+            }
+            continue;
+        }
+
+        if (layer.name === "spawnpoints") {
+            for (const entity of layer.objects) {
+                if (entity.name === "player") {
+                    player.pos = k.vec2(
+                        (map.pos.x + entity.x) * scaleFactor,
+                        (map.pos.y + entity.y) * scaleFactor
+                    );
+                    k.add(player);
+                    continue;
                 }
             }
         }
     }
+
+    k.onUpdate(() => {  
+        k.camPos(player.pos.x, player.pos.y + 100);
+    });
 });
 
 k.go("main");
